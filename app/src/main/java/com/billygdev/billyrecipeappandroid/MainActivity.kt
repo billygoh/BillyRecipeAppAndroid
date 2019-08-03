73047a5964a8496e27b27e447b53ffe29c0aa0d4
Billy <billy>
2019-08-03 18:44:15 +0800

    Add xml file

--- app/src/main/java/com/billygdev/billyrecipeappandroid/MainActivity.kt
+++ app/src/main/java/com/billygdev/billyrecipeappandroid/MainActivity.kt
@@ -2,11 +2,23 @@ package com.billygdev.billyrecipeappandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.xmlpull.v1.XmlPullParser
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val xmlParser: XmlPullParser = this.resources.getXml(R.xml.recipetypes)
+        while (xmlParser.eventType != XmlPullParser.END_DOCUMENT) {
+            if(xmlParser.name == "type") {
+                val name = xmlParser.getAttributeValue(null,"name")
+                if(name != null) {
+                    println("okok::"+name)
+                }
+            }
+            xmlParser.next()
+        }
     }
 }
