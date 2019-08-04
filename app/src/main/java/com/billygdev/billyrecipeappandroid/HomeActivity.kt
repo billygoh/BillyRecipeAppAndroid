fb3221fc2bfd79df5a3202a14482d5be86501833
Billy <billy>
2019-08-04 17:33:01 +0800

    Refactor spinner actionbar

--- app/src/main/java/com/billygdev/billyrecipeappandroid/HomeActivity.kt
+++ app/src/main/java/com/billygdev/billyrecipeappandroid/HomeActivity.kt
@@ -11,6 +11,8 @@ import android.widget.Spinner
 
 class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private val localDB = LocalDB(this)
+    var recipeArr: Array<Recipe> = emptyArray()
+    var ignoreSpinnerFirstCall = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +20,15 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         val sharedPref = this.getSharedPreferences("RECIPE_PREF", Context.MODE_PRIVATE)
         val selectedRecipeTypeID = sharedPref.getInt("selectedRecipeTypeID", 0)
+
+        recipeArr = localDB.getRecipeList(selectedRecipeTypeID)
+        println(":::$recipeArr)")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val sharedPref = this.getSharedPreferences("RECIPE_PREF", Context.MODE_PRIVATE)
+        val selectedRecipeTypeID = sharedPref.getInt("selectedRecipeTypeID", 0)
+
         menuInflater.inflate(R.menu.menu_home, menu)
 
         val item: MenuItem = menu!!.findItem(R.id.filter_action)
@@ -29,12 +37,17 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         spinner.onItemSelectedListener = this
         val arrayAdapter = SpinnerCustomAdapter(this, localDB.showRecipeTypes(), true)
         spinner.adapter = arrayAdapter
+        spinner.setSelection(selectedRecipeTypeID-1)
 
         return true
     }
 
     override fun onItemSelected(av: AdapterView<*>?, view: View?, index: Int, id: Long) {
-        println(":::$id")
+        if(!ignoreSpinnerFirstCall) {
+            recipeArr = localDB.getRecipeList(id.toInt())
+            println(":::$recipeArr)")
+        }
+        ignoreSpinnerFirstCall = false
     }
 
     override fun onNothingSelected(av: AdapterView<*>?) {
